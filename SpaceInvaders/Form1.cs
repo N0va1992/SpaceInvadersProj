9387acdb690aab4b6ded048267c7df9a95b28f6c
Nova92-py <120581606+Nova92-py@users.noreply.github.com>
2023-12-03 16:04:35 +0100

    UserControl added PlayerShip class added

--- SpaceInvaders/Form1.cs
+++ SpaceInvaders/Form1.cs
@@ -1,20 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SpaceInvaders
 {
     public partial class Form1 : Form
     {
+        private PlayerShip playerShip;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            playerShip = new PlayerShip(this); // Przekazujemy referencję do formularza
+            KeyPreview = true;
+            KeyDown += Form1_KeyDown; // Obsługa zdarzenia naciśnięcia klawisza
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Obsługa ruchu statku gracza na podstawie naciśniętego klawisza
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    playerShip.MoveLeft();
+                    break;
+                case Keys.Right:
+                    playerShip.MoveRight();
+                    break;
+                case Keys.Up:
+                    playerShip.MoveUp();
+                    break;
+                case Keys.Down:
+                    playerShip.MoveDown();
+                    break;
+            }
         }
     }
 }
