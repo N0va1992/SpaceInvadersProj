76d8c0c3e188528e9fd09214341b055f5dc523a6
N0va1992 <bekierpatryk311@gmail.com>
2023-12-17 15:55:49 +0100

    player bitmap added

--- SpaceInvaders/StartGameControl.Designer.cs
+++ SpaceInvaders/StartGameControl.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartGameControl));
+            this.playerShip = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerShip)).BeginInit();
             this.SuspendLayout();
+            // 
+            // playerShip
+            // 
+            this.playerShip.Image = ((System.Drawing.Image)(resources.GetObject("playerShip.Image")));
+            this.playerShip.Location = new System.Drawing.Point(476, 512);
+            this.playerShip.Name = "playerShip";
+            this.playerShip.Size = new System.Drawing.Size(134, 90);
+            this.playerShip.TabIndex = 0;
+            this.playerShip.TabStop = false;
             // 
             // StartGameControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.playerShip);
             this.Name = "StartGameControl";
             this.Size = new System.Drawing.Size(1119, 744);
+            ((System.ComponentModel.ISupportInitialize)(this.playerShip)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox playerShip;
     }
 }
